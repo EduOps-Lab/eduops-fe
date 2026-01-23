d1d0d8911745896e5f1d0ceb4cf9a64982b7bb7a
yoorrll <lyl1567@naver.com>
2026-01-23 16:29:24 +0900

    feat: 학생 및 학부모 로그인 페이지 구현

--- src/app/(auth)/learners/login/page.tsx
+++ src/app/(auth)/learners/login/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+import type { LearnerRole, RoleOption } from "@/types/auth.type";
+import LoginForm from "@/components/form/LoginForm";
+import AuthLayout from "@/components/layout/AuthLayout";
+import RoleSelectorBtn from "@/components/button/RoleSelectorBtn";
+
+const LEARNER_ROLES: RoleOption<LearnerRole>[] = [
+  { label: "학생", value: "student" },
+  { label: "학부모", value: "parent" },
+];
+
 export default function LearnersLoginPage() {
-  return <div>LearnersLoginPage</div>;
+  const [selectedRole, setSelectedRole] = useState<LearnerRole>("student");
+
+  return (
+    <AuthLayout
+      title="반갑습니다! 👋"
+      description="학생 또는 학부모를 선택하여 로그인 해주세요."
+    >
+      <RoleSelectorBtn
+        options={LEARNER_ROLES}
+        value={selectedRole}
+        onChange={setSelectedRole}
+      />
+
+      <LoginForm selectedRole={selectedRole} />
+    </AuthLayout>
+  );
 }
